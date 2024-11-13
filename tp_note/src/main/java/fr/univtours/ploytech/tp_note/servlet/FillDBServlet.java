256f926e59546b5ca663ab53b7b7f3cab2442f69
PamplemoussRose <robin49800@gmail.com>
2024-11-13 14:56:34 +0100

    FillDB OK

--- tp_note/src/main/java/fr/univtours/ploytech/tp_note/servlet/FillDBServlet.java
+++ tp_note/src/main/java/fr/univtours/ploytech/tp_note/servlet/FillDBServlet.java
@@ -2,7 +2,8 @@ package fr.univtours.ploytech.tp_note.servlet;
 
 import java.io.IOException;
 
-
+import fr.univtours.ploytech.tp_note.business.BookBusiness;
+import fr.univtours.ploytech.tp_note.model.BookBean;
 import jakarta.inject.Inject;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,31 +15,34 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FillDBServlet extends HttpServlet {
 
     @Inject
-    private NoteBusiness noteBusiness;
+    private BookBusiness bookBusiness;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Ajout de étudiants.
-        StudentBean alice = new StudentBean();
-        alice.setFirstName("Alice");
-        alice.setName("A");
-        StudentBean bob = new StudentBean();
-        bob.setFirstName("Bob");
-        bob.setName("B");
+        // Ajout de livres.
+        BookBean livre1 = new BookBean();
+        livre1.setTitle("Le temps des tempetes");
+        livre1.setAuthor("N. Sarco");
+        livre1.setFree(true);
+        bookBusiness.insertBook(livre1);
 
-        noteBusiness.insertStudent(alice);
-        noteBusiness.insertStudent(bob);
+        BookBean livre2 = new BookBean();
+        livre1.setTitle("Le temps des");
+        livre1.setAuthor("N. Sarco");
+        livre1.setFree(true);
+        bookBusiness.insertBook(livre2);
 
-        // Ajout de notes.
-        NoteBean noteAlice = new NoteBean();
-        noteAlice.setIdStudent(alice.getIdStudent());
-        noteAlice.setNote(20.0F);
-        NoteBean noteBob = new NoteBean();
-        noteBob.setIdStudent(bob.getIdStudent());
-        noteBob.setNote(14.5F);
+        BookBean livre3 = new BookBean();
+        livre1.setTitle("Le temps");
+        livre1.setAuthor("N. Sarco");
+        livre1.setFree(true);
+        bookBusiness.insertBook(livre3);
 
-        noteBusiness.insertNote(noteAlice);
-        noteBusiness.insertNote(noteBob);
+        BookBean livre4 = new BookBean();
+        livre1.setTitle("Le");
+        livre1.setAuthor("N. Sarco");
+        livre1.setFree(false);
+        bookBusiness.insertBook(livre4);
     }
 }
