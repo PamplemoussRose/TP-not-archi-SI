9928a5250858843a168e7f9b7928aa2975ea0735
Burlock2 <gauthier.hamel@gmail.com>
2024-11-13 15:44:42 +0100

    Servlet

--- tp_note/src/main/java/fr/univtours/ploytech/tp_note/servlet/HomeServlet.java
+++ tp_note/src/main/java/fr/univtours/ploytech/tp_note/servlet/HomeServlet.java
@@ -1,8 +1,35 @@
 package fr.univtours.ploytech.tp_note.servlet;
 
+import java.io.IOException;
+import java.util.List;
+
+import fr.univtours.ploytech.tp_note.business.BookBusiness;
+import fr.univtours.ploytech.tp_note.business.BookBusinessImpl;
+import fr.univtours.ploytech.tp_note.model.BookBean;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "homeServlet", urlPatterns = { "/home" })
-public class HomeServlet {
+public class HomeServlet extends HttpServlet {
+    // Dépendances vers les différents services métiers utiles.
+    // Il n'y en a qu'un seul ici.
+    private BookBusiness business;
 
+    @Override
+    public void init() throws ServletException {
+        // Instanciation de ces (cette ici) dépendances.
+        this.business = new BookBusinessImpl();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        List<BookBean> list = this.business.getBookList();
+        req.setAttribute("BOOK_LIST", list);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("bookList.jsp");
+        dispatcher.forward(req, res);
+    }
 }
